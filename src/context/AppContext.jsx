9debf1e14e6d247005e84dd989a0cdb30cda2131
ballesterosDev56 <ballesterosdeveloper56@gmail.com>
2025-05-09 19:15:30 -0500

    Fix QuestionService import path

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer, useCallback } from 'react';
-import {questionService} from '../services/QuestionService';
+import {questionService} from '../services/questionService';
 import ProgressService from '../services/ProgressService';
 import ChatService from '../services/ChatService';
 import OpenAIService from '../services/openaiService';
